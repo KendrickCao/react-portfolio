667960d325c650bf4908122b25ace215443ecbc8
Yanguo Cao <478218222@qq.com>
2022-12-14 00:02:59 +0000

    Finish the hero and update css of header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,36 @@
-import { Container } from "react-bootstrap"
+import { Container,Row,Col,Button } from "react-bootstrap"
+import { FaGithub,FaLinkedinIn,FaFileAlt } from "react-icons/fa"
+import  plumtreeImg  from "../../assets/images/plumtree.png"
 
+import "./Hero.css"
 export const Hero = () => {
-    const style = {
-        backgroundColor:"red",
-        height:"2000px"
-    }
-    return(
-        <>
-            <Container style={style}>
 
+    return(
+        <section className="hero" id="home">
+            <Container>
+                <div className="aligh-items-center">
+                    <h1 className="hero-title">Hi, I'm <span className="linearText">Yanguo.</span><br />
+                    I'm a Full Stack Developer.
+                    </h1>
+                    <div className="connectBtn" onClick={() => console.log('connect')}><span>Let’s Connect</span></div>
+                    {/* Give linear-gradient color to the icons */}
+                    <svg width="0" height="0">
+                        <linearGradient id="color-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+                            <stop stopColor="#02aab0" offset="0%" />
+                            <stop stopColor="#00cdac" offset="100%" />
+                        </linearGradient>
+                    </svg>  
+                    <div className="social-icon">      
+                        <a target="blank" href="https://github.com/KendrickCao/"><FaGithub className="social-icon-svg" style={{ fill: "url(#color-gradient)" }}/></a>
+                        <a target="blank" href="https://www.linkedin.com/in/yanguocao/"><FaLinkedinIn className="social-icon-svg" style={{ fill: "url(#color-gradient)" }}/></a>
+                        <a target="blank" href="https://drive.google.com/file/d/11iSpHAFuoUEUs0MhtJ96e-MClyqBjodM/view?usp=sharing"><FaFileAlt className="social-icon-svg"style={{ fill: "url(#color-gradient)" }}/></a>
+                    </div>
+                </div>
             </Container>
-        </>
+                <div id="island">
+                    <img src={plumtreeImg} alt="plumtree"/>
+                    <div className="background-image-right"></div>
+                </div>
+        </section>
     )
 }
--- src/components/ShortNav/ShortNav.jsx
+++ src/components/ShortNav/ShortNav.jsx
@@ -21,14 +21,15 @@ export const ShortNav = () => {
   const onUpdateActiveLink = (value) => { setActiveLink(value) }
 
     return(
-        <Navbar expand="md" className={scrolled ? "scrolled" : ""} bg="dark" variant="dark">
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
           <Container>
-            <Navbar.Brand href="#home" onClick={() => onUpdateActiveLink('home')}>YANGUO</Navbar.Brand>
+            <Navbar.Brand href="/">YG.C</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
               <span className="navbar-toggler-icon"></span>
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
+                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                 <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
