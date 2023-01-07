65bc7f87b27ea2f56a1220c61eefbbd7da46dd30
Yanguo Cao <478218222@qq.com>
2023-01-07 23:14:07 +0000

    change live link

--- src/components/Portfolio/projectData.js
+++ src/components/Portfolio/projectData.js
@@ -5,7 +5,7 @@ export const projectData = [
         description:"MapLife is a map-based live streaming project. We aim to develop a platform which is easy to explore life all over the world. To satisfy your curiosity, you can explore what’s going on nearby, or see what happened around the world. By collecting geographic data of the live streaming, users can see markers on a worldwide map and join to see live streaming or even be a co-host. Or you can just be the host to share your life with others.",
         tech:["MySQL","Spring Boot","Hibernate","Java","Spring Security","Leaflt.js","Leaflet.markercluster","OpenStreetMap","Twolio","Thymeleaf"],
         gitLink:"https://github.com/KendrickCao/maplife",
-        liveLink:"",
+        liveLink:"https://maplife.yanguo.cf/",
         imgSrc:[
             "https://user-images.githubusercontent.com/93497252/205989238-9c18be14-664a-4341-bfb1-614a229546ef.png",
             "https://user-images.githubusercontent.com/93497252/205992977-1bd0cddd-aa34-4a71-9e1d-2a94ef02d6f6.png",
@@ -17,7 +17,7 @@ export const projectData = [
         description:"It aims to build a co-creative community knowledge website. The website will help communities exchange knowledge and share practices about their projects.",
         tech:["JavaScript","MySQL","Thymeleaf","Maven","Spring Boot","Hibernate","Java","Junit","Webdriver","BootStrap","Terraform","Jenkins","Nginx"],
         gitLink:"https://github.com/KendrickCao/Community-Knowledge",
-        liveLink:"http://communityknowledge.uksouth.cloudapp.azure.com/",
+        liveLink:"https://communityknowledge.yanguo.cf/",
         imgSrc:[
             "https://user-images.githubusercontent.com/93497252/205936233-9a9868ce-7156-4b78-81ae-9a2f3270e55a.png",
             "https://user-images.githubusercontent.com/93497252/205964965-e8fe6b5d-3e61-49f6-97e5-0197151de0e1.png",
