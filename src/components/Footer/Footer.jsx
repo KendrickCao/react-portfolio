1c6a7fbe27c4096d62937100ef202c14bd72bafa
Yanguo Cao <478218222@qq.com>
2023-01-20 00:04:38 +0000

    change pdf cv to google docs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ export const Footer = () =>{
                     <div className="social-icon">      
                         <a title="Github" rel="noreferrer" target="_blank" href="https://github.com/KendrickCao/"><FaGithub className="social-icon-svg" style={{ fill: "#fff" }}/></a>
                         <a title="LinkedIn" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/yanguocao/"><FaLinkedinIn className="social-icon-svg" style={{ fill: "#fff" }}/></a>
-                        <a title="Resume" rel="noreferrer" target="blank" href="https://drive.google.com/file/d/1ZqbMUezQkTWjO-KDDcbSNDl406yxgaIj/view?usp=sharing"><FaFileAlt className="social-icon-svg"style={{ fill: "#fff" }}/></a>
+                        <a title="Resume" rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/1FD15aMrSRAvsK0zhQgegQYhn5vbxbI7u4GO3--VI_I8/edit?usp=sharing"><FaFileAlt className="social-icon-svg"style={{ fill: "#fff" }}/></a>
                     </div>
             </Container>
         </footer>
